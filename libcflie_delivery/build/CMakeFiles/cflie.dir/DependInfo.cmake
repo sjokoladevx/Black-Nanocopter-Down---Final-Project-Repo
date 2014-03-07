59965d59bd5e5cc5f8f94281f1ec86881e19647a
sjokoladevx <stephen.i.prager.14@dartmouth.edu>
2014-03-07 18:12:59 -0500

    Editing gitignore

---
+++ libcflie_delivery/build/CMakeFiles/cflie.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/src/cflie/CCRTPPacket.cpp" "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/build/CMakeFiles/cflie.dir/src/cflie/CCRTPPacket.cpp.o"
+  "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/src/cflie/CCrazyRadio.cpp" "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/build/CMakeFiles/cflie.dir/src/cflie/CCrazyRadio.cpp.o"
+  "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/src/cflie/CCrazyflie.cpp" "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/build/CMakeFiles/cflie.dir/src/cflie/CCrazyflie.cpp.o"
+  "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/src/cflie/CTOC.cpp" "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/build/CMakeFiles/cflie.dir/src/cflie/CTOC.cpp.o"
+  "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/src/leap/leap_controller.cpp" "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/build/CMakeFiles/cflie.dir/src/leap/leap_controller.cpp.o"
+  "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/src/leap/leap_frame.cpp" "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/build/CMakeFiles/cflie.dir/src/leap/leap_frame.cpp.o"
+  "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/src/leap/leap_gesture.cpp" "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/build/CMakeFiles/cflie.dir/src/leap/leap_gesture.cpp.o"
+  "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/src/leap/leap_hand.cpp" "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/build/CMakeFiles/cflie.dir/src/leap/leap_hand.cpp.o"
+  "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/src/leap/leap_pointable.cpp" "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/build/CMakeFiles/cflie.dir/src/leap/leap_pointable.cpp.o"
+  "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/src/leap/leap_utils.cpp" "/Users/stephen/Documents/CollegeSTuff/14W/CS50/project2/libcflie_delivery/build/CMakeFiles/cflie.dir/src/leap/leap_utils.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ libcflie_delivery/build/CMakeFiles/cflie.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cflie.dir/src/cflie/CCrazyRadio.cpp.o"
+  "CMakeFiles/cflie.dir/src/cflie/CCrazyflie.cpp.o"
+  "CMakeFiles/cflie.dir/src/cflie/CCRTPPacket.cpp.o"
+  "CMakeFiles/cflie.dir/src/cflie/CTOC.cpp.o"
+  "CMakeFiles/cflie.dir/src/leap/leap_controller.cpp.o"
+  "CMakeFiles/cflie.dir/src/leap/leap_frame.cpp.o"
+  "CMakeFiles/cflie.dir/src/leap/leap_gesture.cpp.o"
+  "CMakeFiles/cflie.dir/src/leap/leap_hand.cpp.o"
+  "CMakeFiles/cflie.dir/src/leap/leap_pointable.cpp.o"
+  "CMakeFiles/cflie.dir/src/leap/leap_utils.cpp.o"
+  "../lib/libcflie.pdb"
+  "../lib/libcflie.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cflie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
